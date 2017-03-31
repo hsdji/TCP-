5de1b7f1b5a9a99a36849442d45a15a895346925
@单小飞 <shanpengfei@zhidaochuxing.com>
2017-03-31 14:55:24 +0800

    t

--- scoket_客户端/scoket_客户端/CustomerTableViewCell.m
+++ scoket_客户端/scoket_客户端/CustomerTableViewCell.m
@@ -15,7 +15,7 @@
     {
         _message = [[UILabel  alloc]initWithFrame:CGRectMake(0.2*self.frame.size.width, 0, 0.8*self.frame.size.width-20, self.frame.size.height)];
         _message.numberOfLines = 1000;
-        _message.textAlignment = NSTextAlignmentRight;
+        _message.textAlignment = NSTextAlignmentLeft;
     }
       [self.contentView addSubview:_message];
     return _message;
